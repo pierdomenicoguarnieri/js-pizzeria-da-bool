ae6e0c26aaf0e4cb804401ab802c07f740b6ff04
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-11 12:35:35 +0200

    add: function checkVeg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 import menu from "./db.js"
+
+const menuRemap = menu.map(pizza => {
+  const {name, image, price, ingredients, description, votes} = pizza;
+  const isVeg = checkVeg(ingredients);
+  return {name, image, price, ingredients, description, votes, isVeg}
+})
+
+function checkVeg(ingredients) {
+  const noVegIngredients = ["salame", "mozzarella", "salsiccia", "acciughe", "prosciutto", "grana", "scamorza", "gorgonzola"];
+  let isVeg = true;
+  ingredients.forEach(ingredient => {
+    if(noVegIngredients.includes(ingredient)) isVeg = false;
+  });
+  return isVeg;
+}
