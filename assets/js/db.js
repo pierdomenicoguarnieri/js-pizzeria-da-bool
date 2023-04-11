7883f63e65524771816655db4b40d5b684169e0a
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-11 12:12:11 +0200

    add: database

---
+++ assets/js/db.js
@@ -0,0 +1,184 @@
+export default [
+  {
+      name: 'Diavola',
+      image: 'gran-cucina-pizza-diavola.jpg',
+      price: 7,
+      ingredients: [
+          'pomodoro',
+          'salame',
+          'mozzarella'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 3,
+          price: 3,
+          diet: 2
+      }
+  },
+  {
+      name: 'Margherita',
+      image: 'margherita.jpg',
+      price: 6,
+      ingredients: [
+          'pomodoro',
+          'mozzarella',
+          'basilico'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 4,
+          price: 4,
+          diet: 4
+      }
+  },
+  {
+      name: 'Marinara',
+      image: 'Pizza_marinara.jpg',
+      price: 4,
+      ingredients: [
+          'pomodoro',
+          'origano',
+          'aglio'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 3,
+          price: 5,
+          diet: 5
+      }
+  },
+  {
+      name: 'Capricciosa',
+      image: 'pizza-capricciosa.jpg',
+      price: 8,
+      ingredients: [
+          'pomodoro',
+          'mozzarella',
+          'prosciutto',
+          'funghi',
+          'carciofini'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 4,
+          price: 2,
+          diet: 2
+      }
+  },
+  {
+      name: 'Salsiccia',
+      image: 'pizza-con-salsiccia.jpg',
+      price: 7,
+      ingredients: [
+          'pomodoro',
+          'mozzarella',
+          'salsiccia'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 3,
+          price: 2,
+          diet: 1
+      }
+  },
+  {
+      name: 'Margherita Veg',
+      image: 'pizza-margherita-2.jpg',
+      price: 9,
+      ingredients: [
+          'pomodoro',
+          'mozzarella di soia',
+          'basilico'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 2,
+          price: 1,
+          diet: 5
+      }
+  },
+  {
+      name: 'Napoli',
+      image: 'pizza-napoli.jpg',
+      price: 8,
+      ingredients: [
+          'pomodoro',
+          'mozzarella',
+          'acciughe',
+          'capperi'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 1,
+          price: 4,
+          diet: 3
+      }
+  },
+  {
+      name: 'Ortolana',
+      image: 'pizza-ortolana.jpg',
+      price: 8,
+      ingredients: [
+          'pomodoro',
+          'mozzarella',
+          'zucchine',
+          'melanzane',
+          'peperoni'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 4,
+          price: 4,
+          diet: 2
+      }
+  },
+  {
+      name: 'Cocciuto',
+      image: 'Pizza-Vegana-Cocciuto.webp',
+      price: 10,
+      ingredients: [
+          'fiori di zucca',
+          'pomodori secchi',
+          'pesto di basilico'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 5,
+          price: 1,
+          diet: 5
+      }
+  },
+  {
+      name: 'Masanielli',
+      image: 'Pizza-Vegana-I-Masanielli.webp',
+      price: 9,
+      ingredients: [
+          'pomodoro',
+          'insalata',
+          'basilico'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 1,
+          price: 2,
+          diet: 5
+      }
+  },
+  {
+      name: 'Quattro Formaggi',
+      image: 'quattro-formaggi.jpg',
+      price: 8,
+      ingredients: [
+          'mozzarella',
+          'grana',
+          'gorgonzola',
+          'scamorza'
+      ],
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, minima?',
+      votes: {
+          taste: 4,
+          price: 2,
+          diet: 1
+      }
+  }
+];
